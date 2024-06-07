3ff3f92dbc206fe85919d9ca17e664add8372077
Jordi Coll <eliot@Jordis-MacBook-Pro.local>
2024-06-07 23:25:15 +0200

    only run play once

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.selenium_script import create_caos
 from threading import Thread
 
 song_name = 'nothing'
+playing = False
 url = ''
 given_likes = 0;
 total_likes = 0;
@@ -23,22 +24,33 @@ def index():
 
 @app.route('/playing', methods=['GET', 'POST'])
 def playing():
-  play_thread = Thread(target=play)
-  play_thread.start()
-  return render_template('playing.html')
+  global given_likes
+  global total_likes
+  global playing
+  if playing == False:
+    playing = True;
+    given_likes = 0
+    total_likes = 0
+    play_thread = Thread(target=play)
+    play_thread.start()
+    return render_template('playing.html')
+  return render_template('test.html')
+
 
 @app.route('/test', methods=['GET'])
 def test():
   return render_template('test.html')
 
 
-@app.route('/get_playing_stats', methods=['GET'])
+@app.route('get_playing_stats', methods=['GET'])
 def update_playing():
     return jsonify({'song_name': song_name, 'total_likes': total_likes, 'given_likes': given_likes })
 
 def play():
   global given_likes
+  global playing
   for i in range(total_likes):
     create_caos(url, song_name)
     given_likes = i + 1
+  playing = False
   return
